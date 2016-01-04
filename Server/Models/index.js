e6ee553858270cc0eb17ccaccc0f0d9b2c8f21ac
Hack Reactor Students <accounts@hackreactor.com>
2016-01-04 15:48:14 -0800

    Implementing models and updated schema.sql to include stop times table

---
+++ Server/Models/index.js
@@ -0,0 +1,92 @@
+var db = require('../DB');
+
+var listParams = function (array) {
+  var str = '"' + array[0] + '"';  
+  for (var i = 1; i < array.length; i++) {
+    str += ', "' + array[i] + '"';
+  }
+  return str;
+}
+
+var insert = function (table, params, data) {
+  var vals = []
+  for (var i = 0; i < params.length; i++) {
+    output.push(data[params[i]]);
+  }
+  return 'INTERT INTO ' + table + ' (' + listParams(params) + ') VALUES (' + listParams(vals) + ')';
+}
+
+var del = function (table, identifiers) {
+  var str = 'DELETE FROM ' + table + ' WHERE "' + identifiers[0][0] + '" = "' + identifiers[0][1] + '"'
+  for (var i = 1; i < identifiers.length; i++) {
+    str += ' AND WHERE "' + identifiers[i][0] + '" = "' + identifiers[i][1] + '"';
+  }
+} 
+
+var simpleroutesParams = ['route_id', 'route_short_name', 'trip_headsign', 
+            'peak_frequency', 'daytime_frequency', 'offhours_frequency', 'service_start', 'service_end']
+
+module.exports = {
+
+  routes: {
+    get: function (req, res) {
+      var query = "SELECT * FROM simpleroutes";
+      db(query, function (results) {
+        var output = {results: results};
+        output = JSON.stringify(output);
+        res.end(output);
+      })
+    },
+
+    post: function (data, res) {
+      var query = insert('simpleroutes', simpleroutesParams, data);
+      db(query, function () {
+        // Nothing doing
+        res.end('');
+      });
+    },
+
+    // MAKE SURE DATA IS FORMATED CORRECTLY
+    del: function (data, res) {
+      var query = del('simpleroutes', data);
+      db(query, function () {
+        // Nothing doing
+        res.end('');
+      }
+    },
+
+    // put:
+  }
+
+  subway: {
+    get: function (req, res) {
+      
+    },
+
+    post: function (data) {
+
+    },
+
+    del: function (data) {
+
+    },
+
+    // put:
+  }
+
+  // SHOULD HANDLE BUSWAYS??????
+  bus: {
+    get: function (req, res) {
+      
+    }
+
+    post: function (data, res) {
+
+    }
+
+    del: function (data, res) {
+
+    }
+    // put:
+  }
+}
--- Server/schema.sql
+++ Server/schema.sql
@@ -23,3 +23,16 @@ CREATE TABLE routes (
 CREATE TABLE trips (
   route_id INT(11), service_id INT(11), trip_id INT(11), trip_headsign VARCHAR(255), trip_short_name VARCHAR(255), direction_id INT(11), block_id INT(11), shape_id INT(11), PRIMARY KEY (trip_id), FOREIGN KEY (route_id), /* FOREIGN KEY (service_id), FOREIGN KEY (block_id), FOREIGN KEY (shape_id) */
 );
+
+/* trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled,fare_period_id */
+CREATE TABLE stop_times (
+  id INT(11) NOT NULL AUTO_INCREMENT, trip_id INT(11), arrival_time TIME, departure_time TIME, stop_id INT(11), stop_sequence INT(11), stop_headsign VARCHAR(255), pickup_type INT(11), drop_off_type INT(11), shape_dist_traveled NUMBER, fare_period_id INT(11), PRIMARY KEY (id), FOREIGN KEY (trip_id), FOREIGN KEY (stop_id)
+);
+
+/* May prove gratuitous and service spans maybe should be times*/
+CREATE TABLE simpleroutes (
+  id INT(11) NOT NULL AUTO_INCREMENT, route_id INT(11), route_short_name VARCHAR(255), trip_headsign VARCHAR(255), peak_frequency INT(11), daytime_frequency INT(11), offhours_frequency INT(11), service_start INT(11), service_end INT(11), PRIMARY KEY id
+);
+
+
+
