806b127a8eec9f3aaaf83c34a5c70932e3e173e6
Hack Reactor Students <accounts@hackreactor.com>
2016-01-05 13:56:01 -0800

    Same as before

--- Server/app.js
+++ Server/app.js
@@ -8,16 +8,40 @@ var router = require('./router.js');
 
 var staticData = require('/filereader/dataParser.js');
 
+// MOVE THESE FOR FUNCS INTO SERVICES LATER
 var simpleroutesParams = ['route_id', 'route_short_name', 'trip_headsign', 
             'peak_frequency', 'daytime_frequency', 'offhours_frequency', 'service_start', 'service_end'];
 
+var listParams = function (array) {
+  var str = array[0];  
+  for (var i = 1; i < array.length; i++) {
+    str += ', ' + array[i];
+  }
+  return str;
+}
+
+var listValues = function (array) {
+    var str = '"' + array[0] + '"';  
+  for (var i = 1; i < array.length; i++) {
+    str += ', "' + array[i] + '"';
+  }
+  return str;
+};
+
+var insert = function (table, params, data) {
+  return 'INSERT INTO ' + table + ' (' + listParams(params) + ') VALUES (' + listValues(data) + ')';
+}
+
 // INITIIALIZE DATABASE WITH TRANSIT DATA
+// Parse txt files
 var parsedCSVs = staticData.getStaticData();
 
+// Convert static files into simpleroutes format
 var staticSimpleRoutes = staticData.simpleroutes(parsedCSVs);
 
+// insert each simple route, insert into the database
 for (var key in staticSimpleRoutes) {
-  var query = models.insert('simpleroutes', simpleroutesParams, staticSimpleRoutes[key]);
+  var query = insert('simpleroutes', simpleroutesParams, staticSimpleRoutes[key]);
   db(query, function () {
     // Data inserted
   });
